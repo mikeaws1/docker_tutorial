6288db3b35e1485e68b82551ef25580be17cf60c
Michael Alberter <michael.alberter@outlook.de>
2021-03-14 08:27:47 +0100

    Docker-Login hinzugefügt

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,6 @@ echo ${DIGITAL_OCEAN_DROPLET_IP}
 ssh -o StrictHostKeyChecking=no travis@${DIGITAL_OCEAN_DROPLET_IP} << ENDSSH
   touch droplettest
   echo "Test sucessful" > droplettest
+  echo ${1} >> droplettest
+  docker login --username mikeaws1 --password ${1}
 ENDSSH
