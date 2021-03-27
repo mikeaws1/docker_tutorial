2fe16b6eb90d5891bf78b32b64742b95a75d35dd
Michael Alberter <michael.alberter@outlook.de>
2021-03-27 12:06:16 +0100

    Anpassung docker run

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ ssh -o StrictHostKeyChecking=no travis@${DIGITAL_OCEAN_DROPLET_IP} << ENDSSH
   echo $1 >> droplettest
   echo $1 | docker login --username mikeaws1 --password-stdin
   docker stop mikeaws1/docker_tutorial
-  docker run mikeaws1/docker_tutorial -p 3000:3000
+  docker run -p 3000:3000 mikeaws1/docker_tutorial
 ENDSSH
