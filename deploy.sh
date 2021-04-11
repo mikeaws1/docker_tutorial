2ff702ac2e546a6899db1ee771b3d892b0bbef67
Michael Alberter <michael.alberter@outlook.de>
2021-04-11 11:42:20 +0200

    Stop all running containers

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ ssh -o StrictHostKeyChecking=no travis@${DIGITAL_OCEAN_DROPLET_IP} << ENDSSH
   echo "Test sucessful" > droplettest
   echo $1 >> droplettest
   echo $1 | docker login --username mikeaws1 --password-stdin
-  docker stop 8441a1323765
+  docker stop $(docker ps -a -q)
   docker run -d -p 3000:3000 mikeaws1/docker_tutorial
 ENDSSH
