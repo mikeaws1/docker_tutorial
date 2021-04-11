0cb92bfd3359d5deee15f78984196e695aac1f11
Michael Alberter <michael.alberter@outlook.de>
2021-04-11 11:51:54 +0200

    Echo messages added

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@ ssh -o StrictHostKeyChecking=no travis@${DIGITAL_OCEAN_DROPLET_IP} << ENDSSH
   echo "Test sucessful" > droplettest
   echo $1 >> droplettest
   echo $1 | docker login --username mikeaws1 --password-stdin
+  echo "Stopping all"
   docker stop $(docker ps -a -q)
+  echo "Starting up container"
   docker run -d -p 3000:3000 mikeaws1/docker_tutorial
 ENDSSH
